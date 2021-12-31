9ed91715c6e18b834a2d3f394fe80119c69d3dc9
dharsika20 <dharsikarahim@gmail.com>
2021-12-31 13:18:17 +0530

    done

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,34 @@
+package org.company;
+
+public class CompanyInfo {
+	private void companyName() {
+System.out.println("infotech");
+	}
+	private void companyId() {
+System.out.println("12345");
+	}
+	private void companyAddress() {
+System.out.println("chennai");
+	}
+	public static void main(String[]arg) {
+		CompanyInfo c=new CompanyInfo();
+		c.companyName();
+		c.companyId();
+		c.companyAddress();
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+
+
+
+
+
